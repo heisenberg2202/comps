e818a863e748ce305ddeaef48f187b35377b0d12
Ayush <ayushunplugged22@gmail.com>
2023-05-12 17:04:47 +0530

    Till Panel Component

--- src/App.js
+++ src/App.js
@@ -21,9 +21,10 @@ function App() {
   ] 
 
   return (
-    <>
+    <div className="flex"> 
      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </>
+     <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
   );
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { GoChevronDown} from "react-icons/go";
+import Panel from './Panel';
+
 export default function Dropdown({ options, value, onChange }) {
+
   const [isOpen, setIsOpen] = useState(false);
+
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -30,17 +34,17 @@ export default function Dropdown({ options, value, onChange }) {
 
   return (
     <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer "
         onClick={handleClick}
       >
         {value?.label || "Select..."}
         <GoChevronDown className="text-lg"/>
-      </div>
+      </Panel>
       {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+        <Panel className="absolute top-full ">
           {renderOptions}
-        </div>
+        </Panel>
       )}
     </div>
   );
